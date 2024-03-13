33a7b7793adaa8463a1e390d261ec22cc5a5c3d6
Erin2026 <Perrine.wurtz@gmail.com>
2024-03-13 17:21:41 +0100

    do Rhombus

---
+++ src/main/java/me/cyberpals/javart/shapes/simple/Rhombus.java
@@ -0,0 +1,43 @@
+package me.cyberpals.javart.shapes.simple;
+
+import me.cyberpals.javart.vectors.Vector2Float;
+import me.cyberpals.javart.vectors.Vector2Int;
+
+import static java.lang.Math.abs;
+
+public class Rhombus extends SimpleShape{
+    public Rhombus(Vector2Int begin, Vector2Int end) {
+        super(begin, end);
+    }
+
+    public Boolean test(Vector2Int point) {
+        //step 0 - verify if point in bound
+        if (!(
+                point.getX() >= getBegin().getX() &&
+                        point.getX() <= getEnd().getX() &&
+                        point.getY() >= getBegin().getY() &&
+                        point.getY() <= getEnd().getY()
+        )) return false;
+
+        //step 1 - normalize point
+        Vector2Int centeredPoint = point.sub(getBegin());
+        Vector2Float normalizedPoint = new Vector2Float(
+                (((float) centeredPoint.getX()) * 2.f / (end.getX() - begin.getX())) - 1.f,
+                (((float) centeredPoint.getY()) * 2.f / (end.getY() - begin.getY())) - 1.f
+        );
+
+        //step 2 - verify if point in rhombus
+        return abs(normalizedPoint.getX()) + abs(normalizedPoint.getY()) <=1;
+        }
+
+    @Override
+    public void resize(Vector2Int dPos) {
+
+    }
+
+    @Override
+    public void showDetails(int offset) {
+        System.out.println("  ".repeat(offset) + " + Rhombus:(" + getBegin() + "," + getEnd() + ")");
+    }
+}
+
