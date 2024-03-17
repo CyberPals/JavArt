a26254f074225f16cc2124fb3cfb05836ec5f7d6
TheGeekMax <maximesanciaume@gmail.com>
2024-03-17 10:51:26 +0100

    added remaining tests

--- src/main/java/me/cyberpals/javart/vectors/Vector2Int.java
+++ src/main/java/me/cyberpals/javart/vectors/Vector2Int.java
@@ -1,19 +1,19 @@
 package me.cyberpals.javart.vectors;
 
-public class Vector2Int extends Vector2<Integer>  {
+public class Vector2Int extends Vector2<Integer> {
+    public Vector2Int(int a, int b) {
+        super(a, b);
+    }
+
     public Vector2Int add(Vector2<Integer> v) {
         return new Vector2Int(this.getX() + v.getX(), this.getY() + v.getY());
     }
 
     public Vector2Int sub(Vector2<Integer> v) {
-        return new Vector2Int(this.getX()-v.getX(),this.getY()-v.getY());
+        return new Vector2Int(this.getX() - v.getX(), this.getY() - v.getY());
     }
 
     public Vector2Int mult(Vector2<Integer> v) {
-        return new Vector2Int(this.getX()*v.getX(),this.getY()*v.getY());
-    }
-
-    public Vector2Int(int a, int b) {
-        super(a, b);
+        return new Vector2Int(this.getX() * v.getX(), this.getY() * v.getY());
     }
 }
